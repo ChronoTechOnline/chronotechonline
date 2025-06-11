a02dab937b442af473f46decaa612d037b1f45af
Kevin Lopez <129701490+KevLopezTech@users.noreply.github.com>
2025-06-11 02:26:34 -0400

    Starting the base

--- src/data/services.ts
+++ src/data/services.ts
@@ -10,43 +10,42 @@ export type Service = {
 
 export const services: Service[] = [
     {
-        name: "Launchpad Portfolio",
-        price: "Starting at $1,500",
+        name: "Ascend",
+        price: "Starting at $2,000-$4,000",
         details: [
             "Built on the Wix platform for easy user updates.",
-            "Stunning, customized template design.",
-            "Fully mobile-responsive for all devices.",
-            "Basic on-page SEO to get you found.",
+            "Stunning, pre-built or custom template design.",
+            "Create a fully mobile-responsive online presence.",
+            "Perfect for individuals and new businesses to showcase their work and contact information.",
         ],
         icon: 'FiBriefcase', // Icon for a professional portfolio
     },
     {
-        name: "E-commerce Quickstart",
-        price: "Starting at $3,000",
+        name: "Catalyst",
+        price: "Starting at $3,500-$10,000+",
         details: [
             "Powered by Shopify's robust platform.",
             "Payment gateway & shipping configuration.",
-            "Compatible with a wide range of providers."
+            "Compatible with a wide range of providers.",
         ],
         icon: 'FiShoppingCart', // Icon for e-commerce
     },
     {
-        name: "Bespoke Mobile App",
-        price: "Starting at $1,500",
+        name: "Bespoke Mobile",
+        price: "Starting at $15,000-$40,000",
         details: [
             "Android only, iOS pending",
-            "Fluid and responsive UI via React Native",
+            "Developed entirely from scratch using React Native to deliver a seamless and engaging user experience.",
         ],
         icon: 'FiBriefcase', // Icon for a professional portfolio
     },
     {
-        name: "Bespoke Website",
-        price: "Starting at $7,000",
+        name: "Bespoke Web",
+        price: "Starting at $15,000-$20,000",
         details: [
-            "Unique design tailored to your brand identity.",
+            "Unique digital experience tailored to your exact specifications.",
+            "Custom coded from the ground up using React",
             "Blazing-fast performance with Next.js.",
-            "Advanced animations & interactions.",
-            "Headless CMS for easy content management.",
         ],
         icon: 'FiAward', // Icon for premium, custom quality
     },
